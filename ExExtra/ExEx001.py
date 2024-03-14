ed7d5d9022f121daea5dfbc45ed996b976dc3928
Enos Fernandes <kyrotth@gmail.com>
2024-03-14 09:53:54 -0300

    Correção e novo exercício

--- ExExtra/ExEx001.py
+++ ExExtra/ExEx001.py
@@ -1,7 +1,12 @@
 """Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido 
 e continue pedindo até que o usuário informe um valor válido."""
-num = int(input('Digite um número menor ou igual a 10 '))
-if num > 10:
-    int(input('Digite um número menor ou igual a 10 '))
-elif num <= 10:
-    print('Correto!')
+
+while True:
+    nota = float(input("Digite uma nota entre 0 e 10: "))
+    
+    if nota < 0 or nota > 10:
+        print("Valor inválido. Por favor, digite uma nota entre 0 e 10.")
+    else:
+        break
+
+print("Nota válida inserida:", nota)
